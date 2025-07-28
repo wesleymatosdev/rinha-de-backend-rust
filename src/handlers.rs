cf2e1463696abb87ec000d2adbaeb41667a9af36
Wesley Matos <wesleymatosdev@gmail.com>
2025-07-28 08:59:30 -0300

    refactor: decouple handlers from main server file

---
+++ src/handlers.rs
@@ -0,0 +1,102 @@
+use std::collections::HashMap;
+
+use anyhow::Error;
+use async_nats::jetstream::{Context, publish::PublishAck};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use sqlx::Pool;
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct PaymentSummaryFilters {
+    from: Option<DateTime<Utc>>,
+    to: Option<DateTime<Utc>>,
+}
+
+pub async fn publish_payment(context: Context, payment: bytes::Bytes) -> Result<PublishAck, Error> {
+    log::debug!("Publishing payment to NATS: {:?}", payment);
+
+    // Publish the payment to the NATS stream
+    context
+        .publish("payments", payment)
+        .await
+        .expect("Failed to publish payment to NATS")
+        .await
+        .map_err(|e| anyhow::anyhow!("Failed to acknowledge payment: {}", e))
+}
+
+pub async fn payment_summary(
+    filters: PaymentSummaryFilters,
+    pg_pool: Pool<sqlx::Postgres>,
+) -> serde_json::Value {
+    let sql = r#"
+                        SELECT correlation_id, amount, requested_at, gateway
+                        FROM payments
+                        WHERE payments.requested_at >= $1 AND payments.requested_at <= $2
+                    "#;
+
+    let from = filters
+        .from
+        .unwrap_or_else(|| Utc::now() - chrono::Duration::days(30));
+    let to = filters.to.unwrap_or_else(|| Utc::now());
+
+    let payments = sqlx::query_as::<_, (String, f64, DateTime<Utc>, String)>(sql)
+        .bind(from)
+        .bind(to)
+        .fetch_all(&pg_pool)
+        .await
+        .expect("Failed to fetch payment summary");
+
+    let mut summary: HashMap<String, (i32, f64)> = HashMap::new();
+    for (_correlation_id, amount, _requested_at, gateway) in payments {
+        let entry = summary.entry(gateway).or_insert((0, 0.0));
+        entry.0 += 1; // totalRequests
+        entry.1 += amount as f64; // totalAmount
+    }
+
+    let empty_json = serde_json::json!({
+        "totalRequests": 0,
+        "totalAmount": 0.0
+    });
+    let default = summary
+        .get("default")
+        .map(|&(total_requests, total_amount)| {
+            serde_json::json!({
+                "totalRequests": total_requests,
+                "totalAmount": total_amount
+            })
+        })
+        .unwrap_or(empty_json.clone());
+    let fallback = summary
+        .get("fallback")
+        .map(|&(total_requests, total_amount)| {
+            serde_json::json!({
+                "totalRequests": total_requests,
+                "totalAmount": total_amount
+            })
+        })
+        .unwrap_or(empty_json);
+
+    serde_json::json!({
+        "default": default,
+        "fallback": fallback
+    })
+}
+
+pub async fn purge_payments(pg_pool: Pool<sqlx::Postgres>, context: Context) {
+    let sql = "DELETE FROM payments";
+    sqlx::query(sql)
+        .execute(&pg_pool)
+        .await
+        .expect("Failed to purge payments");
+
+    // purge payments from NATS
+    let stream = context
+        .get_stream("payments")
+        .await
+        .expect("Failed to get payments stream");
+
+    stream
+        .purge()
+        .await
+        .expect("Failed to purge payments from NATS");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
+pub mod handlers;
 pub mod payment_processor;
-
-use std::collections::HashMap;
 
 use async_nats::{
     Client,
@@ -12,11 +11,12 @@ use axum::{
     extract::{Query, State},
     routing::{get, post},
 };
-use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
 use sqlx::{Pool, postgres::PgPoolOptions};
 
-use crate::payment_processor::dequeue_payment;
+use crate::{
+    handlers::{PaymentSummaryFilters, payment_summary, publish_payment, purge_payments},
+    payment_processor::dequeue_payment,
+};
 
 async fn get_nats_client() -> Client {
     let nats_url =
@@ -40,12 +40,6 @@ async fn create_pg_pool() -> Result<Pool<sqlx::Postgres>, sqlx::Error> {
         .min_connections(1)
         .connect(&connection_string)
         .await
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct PaymentSummaryFilters {
-    from: Option<DateTime<Utc>>,
-    to: Option<DateTime<Utc>>,
 }
 
 #[derive(Debug, Clone)]
@@ -77,94 +71,23 @@ async fn main() {
         .route(
             "/payments",
             post(|state: State<AppState>, payment: Bytes| async move {
-                state
-                    .context
-                    .publish("payments", payment)
-                    .await
-                    .expect("Failed to publish payment to NATS");
+                let _ = publish_payment(state.context.clone(), payment).await;
             }),
         )
         .route(
             "/payments-summary",
             get(
                 |state: State<AppState>, filters: Query<PaymentSummaryFilters>| async move {
-                    let sql = r#"
-                        SELECT correlation_id, amount, requested_at, gateway
-                        FROM payments
-                        WHERE payments.requested_at >= $1 AND payments.requested_at <= $2
-                    "#;
+                    let response = payment_summary(filters.0.clone(), state.pg_pool.clone()).await;
 
-                    let from = filters
-                        .from
-                        .unwrap_or_else(|| Utc::now() - chrono::Duration::days(30));
-                    let to = filters.to.unwrap_or_else(|| Utc::now());
-
-                    let payments = sqlx::query_as::<_, (String, f64, DateTime<Utc>, String)>(sql)
-                        .bind(from)
-                        .bind(to)
-                        .fetch_all(&state.pg_pool)
-                        .await
-                        .expect("Failed to fetch payment summary");
-                    // }
-
-                    let mut summary: HashMap<String, (i32, f64)> = HashMap::new();
-                    for (_correlation_id, amount, _requested_at, gateway) in payments {
-                        let entry = summary.entry(gateway).or_insert((0, 0.0));
-                        entry.0 += 1; // totalRequests
-                        entry.1 += amount as f64; // totalAmount
-                    }
-
-                    let empty_json = serde_json::json!({
-                        "totalRequests": 0,
-                        "totalAmount": 0.0
-                    });
-                    // solution above doesn't work as it does not have defaults
-                    let default = summary
-                        .get("default")
-                        .map(|&(total_requests, total_amount)| {
-                            serde_json::json!({
-                                "totalRequests": total_requests,
-                                "totalAmount": total_amount
-                            })
-                        })
-                        .unwrap_or(empty_json.clone());
-                    let fallback = summary
-                        .get("fallback")
-                        .map(|&(total_requests, total_amount)| {
-                            serde_json::json!({
-                                "totalRequests": total_requests,
-                                "totalAmount": total_amount
-                            })
-                        })
-                        .unwrap_or(empty_json);
-
-                    axum::Json(serde_json::json!({
-                        "default": default,
-                        "fallback": fallback
-                    }))
+                    axum::Json(response)
                 },
             ),
         )
         .route(
             "/purge-payments",
             post(|state: State<AppState>| async move {
-                let sql = "DELETE FROM payments";
-                sqlx::query(sql)
-                    .execute(&state.pg_pool)
-                    .await
-                    .expect("Failed to purge payments");
-
-                // purge payments from NATS
-                let stream = state
-                    .context
-                    .get_stream("payments")
-                    .await
-                    .expect("Failed to get payments stream");
-
-                stream
-                    .purge()
-                    .await
-                    .expect("Failed to purge payments from NATS");
+                purge_payments(state.pg_pool.clone(), state.context.clone()).await;
 
                 axum::Json("Payments purged successfully")
             }),
